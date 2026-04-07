74e86950a8ca94805020d70cd4978f8fbed80a77
wuxianaisai <wuxianaisai@mail.ru>
2026-04-07 13:24:05 +0300

    clerk webhook endpoint created

--- server/server.ts
+++ server/server.ts
@@ -3,10 +3,13 @@ import express, { Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import { clerkWebhook } from "./controllers/webhooks.js";
 
 const app = express();
 
 await connectDB()
+
+app.post("/api/clerk", express.raw({type: "application/json"}), clerkWebhook)
 
 // Middleware
 app.use(cors())
